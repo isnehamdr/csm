6f8ba9953a9c680ba76b72bd3200d7ed63d9b092
Isneha Manandhar <isnehamanandhar88@gmail.com>
2026-05-06 17:20:38 +0545

    one page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <div className="l">
+        <img src="/images/logo.png" alt="" />
+    </div>
+    
+    </>
+  )
+}
+
+export default Navbar
